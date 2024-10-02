6a9783e7e5d57bc77f1a0b8997ef5d45d31e5dfc
Munilohith Golla <159722405+Munilohith2002@users.noreply.github.com>
2024-10-02 23:29:38 +0530

    Create Level4C_06.c

---
+++ Level4C_06.c
@@ -0,0 +1,12 @@
+// Get a Two digit number from the user and print the reverse of it.
+#include<stdio.h>
+int main()
+{
+int x,y,f,s;
+printf("Enter a TWO digit Number : ");
+scanf("%d",&x);
+f=x%10; //ones
+s=x/10;  //tens
+y=f*10+s;
+printf("The reverse of the entered number is %d\n",y);
+}
