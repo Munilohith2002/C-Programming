ba56b6329cda78da64e0faf49fcdeb81790189a9
Munilohith Golla <159722405+Munilohith2002@users.noreply.github.com>
2024-10-02 23:24:44 +0530

    Create Level4_05.c

---
+++ Level4_05.c
@@ -0,0 +1,11 @@
+//Get a Three digit number from user and print the digit in “Hundreds” position
+
+#include<stdio.h>
+int main()
+{
+int x,y;
+printf("Enter a THREE digit Number : ");
+scanf("%d",&x);
+y=(x/100);
+printf("The digit in Hundreds position is %d\n",y);
+}
