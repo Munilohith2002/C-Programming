26ca2373d871c0709bbd91c7d2e3ba91fbd16154
Munilohith Golla <159722405+Munilohith2002@users.noreply.github.com>
2024-10-03 00:18:26 +0530

    Create Leve4c_07.c

---
+++ Leve4c_07.c
@@ -0,0 +1,14 @@
+// Get a Three digit number from the user and print the reverse of it.
+#include<stdio.h>
+int main()
+{
+int x,y,f,t,h;
+printf("Enter a THREE digit Number : ");
+scanf("%d",&x);
+f=x%10; //ones
+t=(x/10)%10; //tens
+h=x/100; //hundreds
+y=f*100+t*10+h;
+
+printf("The reverse of the entered number is %d\n",y);
+}
