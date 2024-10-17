d7e3d31758549cd84fbf3f3963f704be82ffc9f4
Munilohith Golla <159722405+Munilohith2002@users.noreply.github.com>
2024-10-17 18:41:26 +0530

    Create Level4_014.c

---
+++ Level4_014.c
@@ -0,0 +1,17 @@
+// Write a program to print the total number of single digit odd
+
+// numbers
+#include <stdio.h>
+
+int main() {
+   int count=0;
+   for(int i=0;i<10;i++)
+   {
+       if(i % 2 !=0)
+       {
+           count=count+1;
+       }
+   }
+   printf("the total number of single digit odd :%d \n",count);
+   return 0;
+}
